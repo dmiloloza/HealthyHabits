fdcce29c39145fb7270c16f973c3ecfe45034ee3
dmiloloza <domagoj.miloloza@gmil.com>
2021-02-19 14:19:48 +0100

    html layout idea close #5

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -3,5 +3,17 @@
 
 <main>
   <h1>Healthy Habits</h1>
+  <h2>Today</h2>
+  <span>
+    <h3>Breakfast</h3>
+      <button>Add Meal</button>
+    <h3>Snack</h3>
+      <button>Add Meal</button>
+    <h3>Lunch</h3>
+      <button>Add Meal</button>
+    <h3>Snack</h3>
+      <button>Add Meal</button>
+    <h3>Dinner</h3>
+      <button>Add Meal</button>
+  </span>
 </main>
-
