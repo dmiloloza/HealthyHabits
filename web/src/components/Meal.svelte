26318bb9b8a9c6286675ca171e1130b2a55b7181
dmiloloza <domagoj.miloloza@gmil.com>
2021-02-24 16:57:32 +0100

    feat: fetch breakfast recipes from firebase close #10

---
+++ web/src/components/Meal.svelte
@@ -0,0 +1,19 @@
+<script>
+  //db - firestore database
+  import {db} from '../config/firebase';
+  import MealCard from './MealCard.svelte';
+
+  let meals = [];
+
+  db.collection('breakfast').get()
+  .then(data => {
+    meals = data.docs.map(value => value.data());
+  })
+  .catch(err => console.error(err));
+</script>
+
+<div>
+  {#each meals as meal}
+    <MealCard {...meal} />
+  {/each}
+</div>
