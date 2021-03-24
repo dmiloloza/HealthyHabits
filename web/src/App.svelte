074d00c71a9b117d644eea19a88e980e130bd956
dmiloloza <domagoj.miloloza@gmil.com>
2021-03-24 20:49:46 +0100

    (web) layout components close #21

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,41 +1,40 @@
 <script>
-  import Meals from './components/Meals.svelte';
   import Header from './components/layout/Header.svelte';
   import Footer from './components/layout/Footer.svelte';
 
-  let mealType;
+  /*  let mealType;
 
-  function addMeal(type) {
-    mealType = null;
+    function addMeal(type) {
+      mealType = null;
 
-    setTimeout(() => {
-      showDialog.value = true;
-      mealType = type;
-    });
-  }
+      setTimeout(() => {
+        showDialog.value = true;
+        mealType = type;
+      });
+    }
 
-  let showDialog = {
-    value: false
-  };
+    let showDialog = {
+      value: false
+    };*/
 </script>
 
 <Header />
-{#if mealType}
-  <Meals {mealType} {showDialog} />
-{/if}
-<main>
-  <span>
-    <h3>Doručak</h3>
-      <button on:click={() => addMeal('breakfast')}>Add Meal</button>
-    <h3>Međuobrok</h3>
-     <button on:click={() => addMeal('snack')}>Add Meal</button>
-    <h3>Ručak</h3>
-      <button on:click={() => addMeal('lunch')}>Add Meal</button>
-    <h3>Međuobrok</h3>
-     <button on:click={() => addMeal('snack')}>Add Meal</button>
-    <h3>Večera</h3>
-      <button on:click={() => addMeal('dinner')}>Add Meal</button>
-  </span>
-</main>
+<!--{#if mealType}-->
+<!--  <Meals {mealType} {showDialog} />-->
+<!--{/if}-->
+<!--<main>-->
+<!--  <span>-->
+<!--    <h3>Doručak</h3>-->
+<!--      <button on:click={() => addMeal('breakfast')}>Add Meal</button>-->
+<!--    <h3>Međuobrok</h3>-->
+<!--     <button on:click={() => addMeal('snack')}>Add Meal</button>-->
+<!--    <h3>Ručak</h3>-->
+<!--      <button on:click={() => addMeal('lunch')}>Add Meal</button>-->
+<!--    <h3>Međuobrok</h3>-->
+<!--     <button on:click={() => addMeal('snack')}>Add Meal</button>-->
+<!--    <h3>Večera</h3>-->
+<!--      <button on:click={() => addMeal('dinner')}>Add Meal</button>-->
+<!--  </span>-->
+<!--</main>-->
 
 <Footer />
--- web/src/components/layout/Header.svelte
+++ web/src/components/layout/Header.svelte
@@ -1,1 +1,28 @@
-<h1>Healthy Habits</h1>
+<script>
+  import {Router, Link, Route} from 'svelte-routing';
+  import Home from '../../routes/Home.svelte';
+  import Breakfast from '../../routes/Breakfast.svelte';
+  import Lunch from '../../routes/Lunch.svelte';
+  import Dinner from '../../routes/Dinner.svelte';
+  import Snacks from '../../routes/Snacks.svelte';
+
+  export let url = '';
+</script>
+
+<Router url='{url}'>
+  <nav>
+    <h2>Healthy Habits</h2>
+    <Link to='/'>Početna</Link>
+    <Link to='/breakfast'>Doručak</Link>
+    <Link to='/lunch'>Ručak</Link>
+    <Link to='/dinner'>Večera</Link>
+    <Link to='/snacks'>Međuobrok</Link>
+  </nav>
+  <div>
+    <Route path='/'><Home /></Route>
+    <Route component='{Breakfast}' path='/breakfast' />
+    <Route component='{Lunch}' path='/lunch' />
+    <Route component='{Dinner}' path='/dinner' />
+    <Route component='{Snacks}' path='/snacks' />
+  </div>
+</Router>
