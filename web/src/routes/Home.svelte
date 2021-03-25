07ffbf9b852869fcd1f3ee358abc1a4891606336
dmiloloza <domagoj.miloloza@gmil.com>
2021-03-25 12:57:45 +0100

    (web) Title size fix #27

--- web/src/routes/Home.svelte
+++ web/src/routes/Home.svelte
@@ -1,22 +1,22 @@
 <main>
   <section>
-    <h3>Dručak</h3>
+    <h2>Dručak</h2>
     <img alt='breakfast' src='./images/Savršeni omlet s povrćem.jpg' width='200'>
   </section>
   <section>
-    <h3>Međuobrok</h3>
+    <h2>Međuobrok</h2>
     <img alt='snack' src='./images/Grožđe.jpg' width='200'>
   </section>
   <section>
-    <h3>Ručak</h3>
+    <h2>Ručak</h2>
     <img alt='lunch' src='./images/Pureća prsa s mozzarellom i rajčicom.jpg' width='200'>
   </section>
   <section>
-    <h3>Međuobrok</h3>
+    <h2>Međuobrok</h2>
     <img alt='snack' src='./images/Protein mliječni napitak.jpg' width='200'>
   </section>
   <section>
-    <h3>Večera</h3>
+    <h2>Večera</h2>
     <img alt='dinner' src='./images/Krem juha od bundeve.jpg' width='200'>
   </section>
 </main>
