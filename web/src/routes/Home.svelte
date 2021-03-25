0938ea7a35a6ae3f06d8d59c80d600271e74de2e
dmiloloza <domagoj.miloloza@gmil.com>
2021-03-25 10:34:50 +0100

    (web) home page close #24

--- web/src/routes/Home.svelte
+++ web/src/routes/Home.svelte
@@ -1,1 +1,23 @@
-<h2>Daily specials</h2>
+<main>
+  <h2>Jelovnik</h2>
+  <section>
+    <h3>Dručak</h3>
+    <img src='./images/Savršeni omlet s povrćem.jpg' alt='breakfast' width='200'>
+  </section>
+  <section>
+    <h3>Međuobrok</h3>
+    <img src='./images/Grožđe.jpg' alt='snack' width='200'>
+  </section>
+  <section>
+    <h3>Ručak</h3>
+    <img src='./images/Pureća prsa s mozzarellom i rajčicom.jpg' alt='lunch' width='200'>
+  </section>
+  <section>
+    <h3>Međuobrok</h3>
+    <img src='./images/Protein mliječni napitak.jpg' alt='snack' width='200'>
+  </section>
+  <section>
+    <h3>Večera</h3>
+    <img src='./images/Krem juha od bundeve.jpg' alt='dinner' width='200'>
+  </section>
+</main>
--- web/testMenu.js
+++ web/testMenu.js
@@ -1,134 +1,249 @@
-export const breakfast = [{
-  //izazov1-jelovnik1
-  id: '1-1',
-  category: 'breakfast',
-  description: 'Povrće popecite u tavi na jakoj vatri, s malo maslinovog ulja. Zatim odložite povrće na stranu te ga posolite i popaprite.  U istoj tavi, na malo vrućeg ulja ispecite jaja koja ste prethodno umutili s jogurtom tako da se zapeče vanjska strana (kao palačinke, ali nemojte okretati). U sredinu još polumekog omleta stavite povrće, djelomično preklopite omlet i zatim ga ostavite poklopljenog u vrućoj tavi još 2-3 minute.',
-  image: './images/Savršeni omlet s povrćem.jpg',
-  imageAlt: 'omlet s povrćem',
-  title: 'Savršeni omlet s povrćem',
-  ingredients: [
-    {
-      amount: 2,
-      unit: '',
-      name: 'jaja'
-    },
-    {
-      amount: 40,
-      unit: 'g',
-      name: 'jogurta 2,8% '
-    },
-    {
-      amount: 1,
-      unit: 'žlica ',
-      name: 'ulja'
-    },
-    {
-      amount: 200,
-      unit: 'g',
-      name: 'povrća narezanog na ploške ili rezance (tikvice, patlidžan, luk, paprika i sl.)'
-    },
-    {
-      amount: '',
-      unit: '',
-      name: 'sol, papar'
-    }
-  ]
-}];
-export const lunch = [{
-  //izazov1-jelovnik1
-  id: '1-1',
-  category: 'lunch',
-  description: 'Narežite pureća prsa na odreske, istucite ih batom za meso i posolite. Rajčice očistite, operite i narežite na četvrtine. Istresite ih u zdjelu. Dodajte mozzarellu narezanu na ploške, nasjeckano peršinovo lišće, bosiljak i usitnjeni origano. Posolite, pokapajte maslinovim uljem i sve promiješajte. U velikoj tavi pržite odreske na maslinovu ulju s obje strane. Zalijte ih juhom i pirjajte na laganoj vatri oko 10 minuta. Na svaki odrezak stavite smjesu od rajčice i mozzarelle. Posudu poklopite pa nastavite pirjati na umjerenoj vatri još oko 10 minuta. Povremeno promiješajte i dolijevajte tekućine koliko je potrebno. Poslužite uz kuhani krumpir i zelenu salatu začinjenu s par kapi limunovog soka i žličicom maslinovog ulja.',
-  image: './images/Pureća prsa s mozzarellom i rajčicom.jpg',
-  imageAlt: 'Pureća prsa',
-  title: 'Pureća prsa s mozzarellom i rajčicom',
-  ingredients: [
-    {
-      amount: 150,
-      unit: '',
-      name: 'purećih prsa'
-    },
-    {
-      amount: 2,
-      unit: 'žličice',
-      name: 'maslinovog ulja'
-    },
-    {
-      amount: 50,
-      unit: 'g',
-      name: 'cherry rajčica'
-    },
-    {
-      amount: 50,
-      unit: 'g',
-      name: 'mozzarelle'
-    },
-    {
-      amount: '40 ',
-      unit: 'ml',
-      name: 'bistre juhe'
-    }
-  ]
-}];
-export const dinner = [{
-  //izazov1-jelovnik1
-  id: '1-1',
-  category: 'dinner',
-  description: 'Prepolovite buču, odstranite sjemenke, ogulite koru i nasjeckajte ju na kockice. Preporuka: ako juhu pripremate s Hokkaido bučom, ljusku slobodno možete ostaviti. Sitno nasjeckajte luk. U velikoj posudi zagrijte maslinovo ulje pa na njemu ispirjajte luk, potom dodajte kockice buče i pirjajte nekoliko minuta. Kada je gotovo, zalijte povrtnom juhom i ostavite kuhati 15-20 minuta na srednjoj temperaturi.  Juhu usitnite štapnim mikserom, po želji začinite solju, paprom, muškatnim oraščićem i vrhnjem za kuhanje. Prije serviranja ukrasite peršinom, sjemenkama buče i nekoliko kapi bučina ulja.',
-  image: './images/Krem juha od bundeve.jpg',
-  imageAlt: 'Krem juha',
-  title: 'Krem juha od bundeve',
-  ingredients: [
-    {
-      amount: 125,
-      unit: 'g',
-      name: 'bundeve'
-    },
-    {
-      amount: 250,
-      unit: 'ml',
-      name: 'dmBio povrtne juhe'
-    },
-    {
-      amount: 1,
-      unit: 'žličica',
-      name: 'mBio maslinovog ulja'
-    },
-    {
-      amount: 1 / 4,
-      unit: '',
-      name: 'luka'
-    },
-    {
-      amount: '',
-      unit: '',
-      name: 'sol, papar'
-    },
-    {
-      amount: '',
-      unit: '',
-      name: 'muškatni oraščić'
-    },
-    {
-      amount: 40,
-      unit: 'ml',
-      name: 'vrhnja za kuhanje (po želji)'
-    },
-    {
-      amount: 5,
-      unit: 'g',
-      name: 'bučinih sjemenki'
-    },
-    {
-      amount: '',
-      unit: '',
-      name: 'bučino ulje'
-    }
-  ]
-}];
-export const snacks = [
+//id: '1-1' => izazov1-jelovnik1
+export const breakfast = [
+  {
+    id: '1-1',
+    category: 'breakfast',
+    description: 'Povrće popecite u tavi na jakoj vatri, s malo maslinovog ulja. Zatim odložite povrće na stranu te ga posolite i popaprite.  U istoj tavi, na malo vrućeg ulja ispecite jaja koja ste prethodno umutili s jogurtom tako da se zapeče vanjska strana (kao palačinke, ali nemojte okretati). U sredinu još polumekog omleta stavite povrće, djelomično preklopite omlet i zatim ga ostavite poklopljenog u vrućoj tavi još 2-3 minute.',
+    image: './images/Savršeni omlet s povrćem.jpg',
+    imageAlt: 'omlet s povrćem',
+    title: 'Savršeni omlet s povrćem',
+    ingredients: [
+      {
+        amount: 2,
+        unit: '',
+        name: 'jaja'
+      },
+      {
+        amount: 40,
+        unit: 'g',
+        name: 'jogurta 2,8%'
+      },
+      {
+        amount: 1,
+        unit: 'žlica',
+        name: 'ulja'
+      },
+      {
+        amount: 200,
+        unit: 'g',
+        name: 'povrća narezanog na ploške ili rezance (tikvice, patlidžan, luk, paprika i sl.)'
+      },
+      {
+        amount: '',
+        unit: '',
+        name: 'sol, papar'
+      }
+    ]
+  },
+  {
+    id: '1-2',
+    category: 'breakfast',
+    description: 'Kukuruznu krupicu skuhajte u 140 ml vode. Sir malo posolite i pomiješajte s uljem te servirajte uz palentu.Kukuruznu krupicu skuhajte u 140 ml vode. Sir malo posolite i pomiješajte s uljem te servirajte uz palentu.',
+    image: './images/Palenta s posnim sirom.jpg',
+    imageAlt: 'Palenta',
+    title: 'Palenta s posnim sirom',
+    ingredients: [
+      {
+        amount: 150,
+        unit: 'g',
+        name: 'posnog mekog kravljeg sira'
+      },
+      {
+        amount: 1,
+        unit: 'jušna žlica',
+        name: 'maslinovog ulja'
+      },
+      {
+        amount: 35,
+        unit: 'g',
+        name: 'kukuruzne krupice (140 ml vode)'
+      },
+      {
+        amount: '',
+        unit: '',
+        name: 'sol'
+      }
+    ]
+  },
+];
+export const lunch = [
   {
     //izazov1-jelovnik1
+    id: '1-1',
+    category: 'lunch',
+    description: 'Narežite pureća prsa na odreske, istucite ih batom za meso i posolite. Rajčice očistite, operite i narežite na četvrtine. Istresite ih u zdjelu. Dodajte mozzarellu narezanu na ploške, nasjeckano peršinovo lišće, bosiljak i usitnjeni origano. Posolite, pokapajte maslinovim uljem i sve promiješajte. U velikoj tavi pržite odreske na maslinovu ulju s obje strane. Zalijte ih juhom i pirjajte na laganoj vatri oko 10 minuta. Na svaki odrezak stavite smjesu od rajčice i mozzarelle. Posudu poklopite pa nastavite pirjati na umjerenoj vatri još oko 10 minuta. Povremeno promiješajte i dolijevajte tekućine koliko je potrebno. Poslužite uz kuhani krumpir i zelenu salatu začinjenu s par kapi limunovog soka i žličicom maslinovog ulja.',
+    image: './images/Pureća prsa s mozzarellom i rajčicom.jpg',
+    imageAlt: 'Pureća prsa',
+    title: 'Pureća prsa s mozzarellom i rajčicom',
+    ingredients: [
+      {
+        amount: 150,
+        unit: '',
+        name: 'purećih prsa'
+      },
+      {
+        amount: 2,
+        unit: 'žličice',
+        name: 'maslinovog ulja'
+      },
+      {
+        amount: 50,
+        unit: 'g',
+        name: 'cherry rajčica'
+      },
+      {
+        amount: 50,
+        unit: 'g',
+        name: 'mozzarelle'
+      },
+      {
+        amount: '40 ',
+        unit: 'ml',
+        name: 'bistre juhe'
+      }
+    ]
+  },
+  {
+    //izazov1-jelovnik1
+    id: '1-2',
+    category: 'lunch',
+    description: 'Mahune očistite, narežite na prutiće pa blanširajte u posoljenoj vodi. Papriku narežite na manje prutiće, a luk na tanko. Pileći file narežite na što tanje trakice pa marinirajte u soja umaku. U zagrijani wok dodajte ulje, zatim pileći file pa svo povrće i slatki čili umak. Pržite što kraće jer povrće mora ostati hrskavo. Poslužite uz rižine rezance koje ste skuhali u posoljenoj vodi 1-2 minute.',
+    image: './images/Kineski rižini rezanci sa slatkim čili umakom i piletinom.jpg',
+    imageAlt: 'Rezanci i piletina',
+    title: 'Kineski rižini rezanci sa slatkim čili umakom i piletinom',
+    ingredients: [
+      {
+        amount: 110,
+        unit: 'g',
+        name: 'pilećeg filea'
+      },
+      {
+        amount: 75,
+        unit: 'g',
+        name: 'rižinih rezanaca'
+      },
+      {
+        amount: 40,
+        unit: 'g',
+        name: 'mahuna'
+      },
+      {
+        amount: 40,
+        unit: 'g',
+        name: 'mladog luka'
+      },
+      {
+        amount: 1 / 4,
+        unit: '',
+        name: 'crvene paprike'
+      },
+      {
+        amount: 25,
+        unit: 'g',
+        name: 'sojinih klica'
+      },
+      {
+        amount: 25,
+        unit: 'g',
+        name: 'bambusovih stabljika'
+      },
+      {
+        amount: '',
+        unit: '',
+        name: 'slatki čili umak, soja, ulje'
+      }
+    ]
+  },
+];
+export const dinner = [
+  {
+    id: '1-2',
+    category: 'dinner',
+    description: 'Prepolovite buču, odstranite sjemenke, ogulite koru i nasjeckajte ju na kockice. Preporuka: ako juhu pripremate s Hokkaido bučom, ljusku slobodno možete ostaviti. Sitno nasjeckajte luk. U velikoj posudi zagrijte maslinovo ulje pa na njemu ispirjajte luk, potom dodajte kockice buče i pirjajte nekoliko minuta. Kada je gotovo, zalijte povrtnom juhom i ostavite kuhati 15-20 minuta na srednjoj temperaturi.  Juhu usitnite štapnim mikserom, po želji začinite solju, paprom, muškatnim oraščićem i vrhnjem za kuhanje. Prije serviranja ukrasite peršinom, sjemenkama buče i nekoliko kapi bučina ulja.',
+    image: './images/Krem juha od bundeve.jpg',
+    imageAlt: 'Krem juha',
+    title: 'Krem juha od bundeve',
+    ingredients: [
+      {
+        amount: 125,
+        unit: 'g',
+        name: 'bundeve'
+      },
+      {
+        amount: 250,
+        unit: 'ml',
+        name: 'dmBio povrtne juhe'
+      },
+      {
+        amount: 1,
+        unit: 'žličica',
+        name: 'mBio maslinovog ulja'
+      },
+      {
+        amount: 1 / 4,
+        unit: '',
+        name: 'luka'
+      },
+      {
+        amount: '',
+        unit: '',
+        name: 'sol, papar'
+      },
+      {
+        amount: '',
+        unit: '',
+        name: 'muškatni oraščić'
+      },
+      {
+        amount: 40,
+        unit: 'ml',
+        name: 'vrhnja za kuhanje (po želji)'
+      },
+      {
+        amount: 5,
+        unit: 'g',
+        name: 'bučinih sjemenki'
+      },
+      {
+        amount: '',
+        unit: '',
+        name: 'bučino ulje'
+      }
+    ]
+  },
+  {
+    id: '1-2',
+    category: 'dinner',
+    description: 'Krupnijim šampinjonima maknite stabljike, a klobuke naglo popecite sa svih strana na malo maslinova ulja. Maknite s vatre, lagano posolite i popaprite, nabodite na štapiće i premjestite u posudu za pečenje. Udubine napunite svježim krem sirom i zapecite u prethodno zagrijanoj pećnici na 200 °C oko pet minuta. Poslužite ih vruće, posute svježim nasjeckanim začinskim biljem.',
+    image: './images/Zapečeni šampinjoni sa svježim krem sirom.jpg',
+    imageAlt: 'Zapečeni šampinjoni',
+    title: 'Zapečeni šampinjoni sa svježim krem sirom',
+    ingredients: [
+      {
+        amount: 1 / 4,
+        unit: 'žlice',
+        name: 'maslinovog ulja'
+      },
+      {
+        amount: 100,
+        unit: 'g',
+        name: 'šampinjona'
+      },
+      {
+        amount: 60,
+        unit: 'g',
+        name: 'svježeg krem sira'
+      },
+      {
+        amount: '',
+        unit: '',
+        name: 'sol, papar, začinsko bilje (vlasac, peršin i mažuran'
+      }
+    ]
+  },
+];
+export const snacks = [
+  {
     id: '1-1',
     category: 'snacks',
     description: '',
@@ -144,13 +259,13 @@ export const snacks = [
     ]
   },
   {
-    //izazov1-jelovnik1
+    // 1-1a => second snack in a day
     id: '1-1a',
     category: 'snacks',
     description: '',
-    image: './images/z\' bregov Protein mliječni napitak.jpg',
+    image: './images/Protein mliječni napitak.jpg',
     imageAlt: 'Grožđe',
-    title: 'Grožđe',
+    title: 'z\' bregov Protein mliječni napitak',
     ingredients: [
       {
         amount: 250,
@@ -158,4 +273,36 @@ export const snacks = [
         name: 'z bregov Protein mliječni napitak'
       }
     ]
-  }  ];
+  },
+  {
+    id: '1-2',
+    category: 'snacks',
+    description: '',
+    image: './images/dmBio mješavina orašastih plodova.png',
+    imageAlt: 'Orašasti plodovi',
+    title: 'Mješavina orašastih plodova',
+    ingredients: [
+      {
+        amount: 10,
+        unit: 'g',
+        name: 'mješavine orašastih plodova'
+      }
+    ]
+  },
+  {
+    // 1-1a => second snack in a day
+    id: '1-2a',
+    category: 'snacks',
+    description: '',
+    image: './images/Banana.jpg',
+    imageAlt: 'Banana',
+    title: 'Banana',
+    ingredients: [
+      {
+        amount: 1,
+        unit: '',
+        name: 'banana'
+      }
+    ]
+  },
+];
