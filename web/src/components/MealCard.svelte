91ec4c70b2dfadf55e8335fa7043d8ded4e0ca30
dmiloloza <domagoj.miloloza@gmil.com>
2021-03-25 12:40:52 +0100

    (web) add to meal plan button #27

--- web/src/components/MealCard.svelte
+++ web/src/components/MealCard.svelte
@@ -1,20 +1,15 @@
 <script>
-  export let category;
-  // export let imgAlt;
-  // export let image;
-  // export let title;
-  // export let description;
-  // export let ingredients = {};
-
+  export let meals;
 </script>
 
-<main>
-  <img alt='{category.imageAlt}' src='{category.image}' width='200' />
-  <h2>{category.title}</h2>
-  <p><b>Priprema:</b> {category.description}</p>
-  {#each ingredients as ingredient }
+{#each meals as meal }
+  <h2>{meal.title}</h2>
+  <img alt='{meal.imageAlt}' src='{meal.image}' width='200' />
+  <p><b>Priprema:</b> {meal.description}</p>
+  {#each meal.ingredients as ingredient }
     <ul>
       <li>{ingredient.amount} {ingredient.unit} {ingredient.name}</li>
     </ul>
   {/each}
-</main>
+  <button>Dodaj u jelovnik</button>
+{/each}
