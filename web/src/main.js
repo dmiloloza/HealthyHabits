76b484ecc7e1224f2f34a390fcd133f8d689d785
dmiloloza <domagoj.miloloza@gmil.com>
2021-03-26 17:34:15 +0100

    (web) IBM carbon design init close #30

--- web/src/main.js
+++ web/src/main.js
@@ -1,4 +1,5 @@
 import App from './App.svelte';
+import "carbon-components-svelte/css/all.css";
 
 //all html content from App.svelte goes to 'public/index.html' => <body> tag
 // App.svelte is like root component. It's top level component. We nest all other components inside it.
